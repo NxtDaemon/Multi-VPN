be8fdbbb39c657823d69dd2545e0aefc05bd135a
Luke Cotterell <44589876+NxtDaemon@users.noreply.github.com>
2021-05-23 01:07:52 +0100

    Adding Color to Display with ANSI codes

--- Setup.py
+++ Setup.py
@@ -6,27 +6,48 @@ import sys
 # Run with Sudo as it makes PermissionErrors otherwise 
 # Syntax `sudo python3 setup.py` 
 
+class Color:
+	# If you wish to use custom colors add them here and change the varibles below
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLINKING = '\033[5m'
+    RESET = '\033[0m'
+
+ActiveColor = Color.GREEN
+QuestionColor = Color.BOLD+Color.YELLOW
+ErrorColor = Color.RED
+NumColor = Color.PURPLE+Color.BOLD
+NotificationColor = Color.CYAN
+RESET = Color.RESET
+
 VPNFiles = sys.argv
 VPNFiles.pop(0)
 
 FilesInOpt = os.listdir("/opt/")
 if "VPNs"  in FilesInOpt:
-	print("Error : there is already a /opt/VPNs/ Directory")
+	print(f"{ErrorColor}Error : there is already a /opt/VPNs/ Directory{RESET}")
 else:
 	try:
 		os.mkdir("/opt/VPNs")
-		print("/opt/VPNs/ Directory Successfully Created")
+		print(f"{ActiveColor}Directory Successfully Created at `/opt/VPNs/`{RESET}")
 		if VPNFiles:
-			print("Moving VPN Files Now")
+			print(f"{ActiveColor}Moving VPN Files Now{RESET}")
 			for file in VPNFiles: # Will also need sudo privs but due to the catch before shouldn't cause an issue
 				src = file
 				destination = "/opt/VPNs/"
 				shutil.copy(file,destination)
-			print("Successfully Copied VPN Files")
+			print(f"{ActiveColor}Successfully Copied VPN Files{RESET}")
 
 	except Exception as Exc:
 		Content = ""
 		if type(Exc) == PermissionError:
 			Content += "\nDid you forget to add `sudo`?"
-		print(f"CRITICAL : Following Error was Found '{Exc}'{Content}")
+		print(f"{ErrorColor}CRITICAL : Following Error was Found '{Exc}'{Content}{RESET}")
 		exit()
--- VPN.py
+++ VPN.py
@@ -1,22 +1,52 @@
 import os
 import subprocess 
 
+class Color:
+	# If you wish to use custom colors add them here and change the varibles below
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLINKING = '\033[5m'
+    RESET = '\033[0m'
+
+ActiveColor = Color.GREEN
+QuestionColor = Color.BOLD+Color.YELLOW
+ErrorColor = Color.RED
+NumColor = Color.PURPLE+Color.BOLD
+NotificationColor = Color.CYAN
+RESET = Color.RESET
+
+
 def ChooseVPN():
+	'return the value of the chosen .ovpn file' 
 	VPNs = os.listdir("/opt/VPNs")
 	for value in enumerate(VPNs,1):
 		if not value[1].endswith(".ovpn"):
 			pass
 		else:
-			print(f"[{value[0]}] : {value[1]}")
+			print(f"{NumColor}[{value[0]}]{RESET} : {ActiveColor}{value[1]}{RESET}")
 	print("\n[?] : Input the VPN Number you wish to use \n ")
 	while 1:
-		Selected = int(input("> "))-1
-		if VPNs[Selected]:
+		try:
+			Selected = int(input(f"{QuestionColor}>{RESET} "))-1
 			return(VPNs[Selected]) 
-		else:
-			print("Sorry, The Number you Entered is not in the VPNs List")
+		except IndexError:
+			print(f"{ErrorColor}Sorry, The Number you Entered is not in the VPNs List{RESET}")
+		except ValueError:
+			print(f"{ErrorColor}Input Must be Numeric; Enter a Number{RESET}")
+		except EOFError:
+			print(f"{ErrorColor}Found `Ctrl+D` use Ctrl+C {RESET}")
+		except Exception as Exc:
+			print(f"{ErrorColor}Unexpected Error : {Exc} Class : {type(Exc).__name__}{RESET}")
 
 def SetVPN(VPN):
+	'Call the openvpn command and handle commands'
 	Process = subprocess.call(["sudo","openvpn",f"/opt/VPNs/{VPN}"])
 
 if __name__ == "__main__":
@@ -26,6 +56,7 @@ if __name__ == "__main__":
 	# '$(Username) ALL=(all) NOPASSWD: $(which openvpn)'
 	# But ofc replace username and the value from 'which openvpn'
 	# Also imo its a nice idea to add an alias for this so add something like `alias vpn="python3 $(location of VPN.py)"
-	print("Starting VPN Now \n")
+	print(f"{NotificationColor}Starting VPN Now \n{RESET}")
 	SetVPN(VPN)
+	print(f"{Color.RESET}")
 
